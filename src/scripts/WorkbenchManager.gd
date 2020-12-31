b17fc6386eecf08f212ed94ec3bcb2d6dd81610d
Marcus idpd320 <marcusizabel230@gmail.com>
2020-12-31 12:16:20 -0300

    saving with wrong name fix

--- src/scripts/WorkbenchManager.gd
+++ src/scripts/WorkbenchManager.gd
@@ -3,5 +3,5 @@ extends Node
 var workbench : GraphEdit
 
 func add_gate(gate : Gate):
-	workbench.add_child(gate)
+	workbench.add_child(gate, true)
 	gate.offset = workbench.scroll_offset + (workbench.rect_size / 2)
